efcaf533292632a1641b8fd0a7660328336ff2f4
Thomas <tg.59290@gmail.com>
2019-06-05 11:58:17 +0200

    can delete a thought

--- app/views.py
+++ app/views.py
@@ -11,7 +11,7 @@ def index():
     thoughts = Thought.query.all()
     return render_template("index.html.j2", thoughts=thoughts)
 
-@app.route('/thoughts/new', methods=['GET', 'POST'])
+@app.route('/admin/thought/new', methods=['GET', 'POST'])
 def new_thought():
     form = NewThoughtForm()
     if form.validate_on_submit():
@@ -19,7 +19,20 @@ def new_thought():
         db.session.add(thought)
         db.session.commit()
         return redirect('/index')
-    return render_template("new_thought.html.j2", form=form)
+    return render_template("admin/new_thought.html.j2", form=form)
+
+@app.route('/admin/thoughts/')
+def thoughts():
+    thoughts = Thought.query.all()
+    return render_template("admin/thoughts.html.j2", thoughts = thoughts)
+
+@app.route('/admin/thought/delete/<int:id>')
+def delete_thought(id):
+    thought = Thought.query.get(id)
+    db.session.delete(thought)
+    db.session.commit()
+    return redirect('/admin/thoughts/')
+
 
 if __name__ == "__main__":
     app.run()
