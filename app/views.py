bd50d74d2d1a7d40c545fcbf13f3978188b69ee6
Thomas <tg.59290@gmail.com>
2019-06-05 14:40:36 +0200

    can update a thought

--- app/views.py
+++ app/views.py
@@ -11,6 +11,12 @@ def index():
     thoughts = Thought.query.all()
     return render_template("index.html.j2", thoughts=thoughts)
 
+
+@app.route('/admin/thoughts/')
+def thoughts():
+    thoughts = Thought.query.all()
+    return render_template("admin/thoughts.html.j2", thoughts = thoughts)
+
 @app.route('/admin/thought/new', methods=['GET', 'POST'])
 def new_thought():
     form = NewThoughtForm()
@@ -21,17 +27,22 @@ def new_thought():
         return redirect('/index')
     return render_template("admin/new_thought.html.j2", form=form)
 
-@app.route('/admin/thoughts/')
-def thoughts():
-    thoughts = Thought.query.all()
-    return render_template("admin/thoughts.html.j2", thoughts = thoughts)
-
 @app.route('/admin/thought/delete/<int:id>')
 def delete_thought(id):
     thought = Thought.query.get(id)
     db.session.delete(thought)
     db.session.commit()
     return redirect('/admin/thoughts/')
+
+@app.route('/admin/thought/update/<int:id>', methods=['GET', 'POST'])
+def update_thought(id):
+    thought = Thought.query.get(id)
+    form = NewThoughtForm(obj=thought)
+    if form.validate_on_submit():
+        thought.content = form.content.data
+        db.session.commit()
+        return redirect('/admin/thoughts/')
+    return render_template('admin/update_thought.html.j2', thought=thought, form=form)
 
 
 if __name__ == "__main__":
