bbc23409e15573489501cdee298f4b679b2993a8
Thomas <tg.59290@gmail.com>
2019-06-07 14:40:47 +0200

    changed config.py for production

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+# Database initialization
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
