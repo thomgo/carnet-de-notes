ffbd763f859c14ab76359462c7dd17ce5379c96e
Thomas <tg.59290@gmail.com>
2019-06-07 12:09:29 +0200

    add session duration of 30 minutes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+from datetime import timedelta
+
+from flask import Flask, session
 from flask_login import LoginManager
 
 from .views import app
@@ -10,6 +12,16 @@ models.db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+login_manager.refresh_view = 'login'
+login_manager.refresh_message = (u"Votre session a expiré")
+login_manager.refresh_message_category = "warning"
+
+
+@app.before_request
+def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=30)
+
 
 @app.cli.command("init_db")
 def init_db():
